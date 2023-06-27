62b05ef5505e5f53ee61bb465f48a82db5d9bbe8
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-27 19:55:34 +0300

    Update Main.java

--- src/main/java/com/glorry/Main.java
+++ src/main/java/com/glorry/Main.java
@@ -2,10 +2,11 @@ package com.glorry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Main {
-
+    @GetMapping("/")
     public String greet(){
         return "Hello";
     }
