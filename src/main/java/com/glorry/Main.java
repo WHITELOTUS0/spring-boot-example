e02b596482c17c296588f96c57575ca0906174ad
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-27 20:21:07 +0300

    Update Main.java

--- src/main/java/com/glorry/Main.java
+++ src/main/java/com/glorry/Main.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Main {
+
+    record GreetResponse(String greet){}
     @GetMapping("/")
     public String greet(){
         return "Hello";
