7205c809ee3f8d9f5cafa6443c33b030c3209535
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-27 21:03:35 +0300

    Update Customer.java

--- src/main/java/com/glorry/Customer.java
+++ src/main/java/com/glorry/Customer.java
@@ -1,4 +1,17 @@
 package com.glorry;
 
 public class Customer {
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Customer(Integer id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+
 }
