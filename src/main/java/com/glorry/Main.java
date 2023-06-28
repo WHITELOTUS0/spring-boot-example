6a320d0f5271aeb709880a2801781e3de2fd2556
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-28 17:55:00 +0300

    Update Main.java

--- src/main/java/com/glorry/Main.java
+++ src/main/java/com/glorry/Main.java
@@ -3,6 +3,7 @@ package com.glorry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,6 +55,7 @@ public class Main {
     ){
 
     }
+    @PostMapping
     public void addCustomer(NewCustomerRequest request){
         Customer customer = new Customer();
         customer.setName(request.name());
